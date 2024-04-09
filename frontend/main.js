d5b50045b0a900a764e36dba0e3862e46c85b72b
ZurielRedux <mjfolwarski@gmail.com>
2024-04-08 18:33:16 -0700

    fix: Corrected an issue with HTTPS and a typo in index

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', event => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://zurielpythonfunctionapp.azurewebsites.net/api/GetResumeCounter?';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(Response => {
+    fetch(functionApiUrl).then(Response => {
         return Response.json()
     }).then(Response => {
         console.log("Website called function API.");
